d30ae919fe856d929a7a3c5cc192b5d4eb0f0cc9
yangpeng <yangpengzhanghome@gmail.com>
2025-08-23 12:52:39 -0700

    Week1 Day2 update

---
+++ Exercises/Week1/Day2.py
@@ -0,0 +1,10 @@
+# Question: Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. 
+
+num = input("Choose an number between 1 and 100:")
+if float(num) - int(num) == 0.0:
+    if int(num)%2 == 0:
+        print("This number is even")
+    else:
+        print("This number is odd")
+else:
+    print("The number you input is not an integer.")
